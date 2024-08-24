cfa054f6bede323daacd02f5a20466648aa12f40
Feecon <mail2feeconbehera@gmail.com>
2024-08-24 17:04:31 +0530

    static site pointed.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
@@ -7,55 +6,43 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const messageRouter = require("./routers/messageRouter");
 const userRouter = require("./routers/userRouter");
-// const productRouter = require("./routers/productRouter");
 
 app.use(express.json());
+// Serve static files from the Vite build directory
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// For any request that doesn't match an API route, serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 const PORT = process.env.PORT || 3300;
 const DB_URI = process.env.DB_URI;
 
-//middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-/**Database connection starts */
+/** Database connection starts */
 mongoose
   .connect(DB_URI)
   .then(() => {
     console.log("Connection to MongoDB is established!");
   })
   .catch((err) => {
-    console.log("something went wrong with DB connection", err);
+    console.log("Something went wrong with DB connection", err);
   });
 
-/**Database connection ends */
-
-
-/**routers*/
+/** Routers */
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
 
-//fallback middleware, i.e if no middleware works then the default middleware works
+// Fallback middleware for 404 errors
 app.use(function (req, res) {
   res.status(404).send("404 Not Found");
 });
 
-// //serve static assets if in production
-
-
-// app.use(express.static(path.join(__dirname, "../client/dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-// });
-
-// Serve static files from the Vite build directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
-
-
-//start the server
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost/${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
